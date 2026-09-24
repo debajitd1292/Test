9fbfc182d25690494671a1e9853bd13d878190f1
Debajit <debajit.bcpl@gmail.com>
2026-09-24 18:05:19 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = "ppu-test-cache-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./shift.csv",
+  "./holiday.csv",
+  "./notes.csv",
+  "./logo.png",
+  "./manifest.json"
+];
+
+// Install - cache files
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// Activate - clean old cache
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch - network first, fallback to cache
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        // Update cache dynamically
+        const resClone = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, resClone);
+        });
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
